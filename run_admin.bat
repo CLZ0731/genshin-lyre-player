b3373e1cdc33879b52b7ac6841da10fd012b6611
CLZ0731 <Mark19980731@gmail.com>
2026-07-16 21:27:05 +0800

    Upload run_admin.bat via API

---
+++ run_admin.bat
@@ -0,0 +1,25 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+echo Starting Genshin Lyre Player...
+set "PYTHON_PATH=C:\Users\mart\AppData\Local\Programs\Python\Python312\python.exe"
+
+"%PYTHON_PATH%" main.py
+
+echo.
+echo Program has exited.
+pause
